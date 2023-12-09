91bd79aacf0506535fe443051f9655f5221f7487
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 03:29:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+ 
+ 
+// Question 01 
+ var username = "SALMAN"  
+
+alert(username)
+// Question 02
+var firstname = "salman "
+
+var secondname = "yaseen"
+
+var fullname = firstname+secondname
+
+
+alert(fullname)
+
+// Question no 3
+var js ="message"
+alert(js)
+
+// Question no 4
+
+// Question 05
+
+alert ("pizza\npizz\nPiz\nPi\nP")
+
+// Question 06
+var email = "My email address is example@xyzcom"
+
+alert(email)
+
+// Question no 7
+var book ="A smarter way to learn JavaScript"
+alert(book);
+
+var message ="Yah! I can write HTML content through JavaScript"
+document.write(message);
+
+// Question no 9
+
+var design = "▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)   
+document.write("<br>"+design)
